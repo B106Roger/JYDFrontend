256320e1865ef1c63ca98953f01b0e8f3ad01fdd
NeroSilver <robert93327@gmail.com>
2019-06-30 22:56:31 +0800

    路由設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,31 @@ const routes: Routes = [
   {
     path: 'lobby',
     component: LobbyComponent
+  },
+  {
+    path: '**',
+    redirectTo : ''
   }
 ];
-
+/*
+ Route {
+    path?: string //瀏覽器上方的網址列的字串
+    pathMatch?: string //當導航至此網址時要顯示的元件
+    matcher?: UrlMatcher //網址列過濾器
+    component?: Type
+    redirectTo?: string //要轉址到那邊
+    outlet?: string
+    canActivate?: any[]
+    canActivateChild?: any[]
+    canDeactivate?: any[]
+    canLoad?: any[]
+    data?: Data //要傳入元件裡的資料
+    resolve?: ResolveData
+    children?: Routes
+    loadChildren?: LoadChildren
+    runGuardsAndResolvers?: RunGuardsAndResolvers
+  }
+*/
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
