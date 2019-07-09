c2912ef9d6be5b06b9fc3d6271673f48bb2aec91
106roger <imbigking12@gmail.com>
2019-07-10 00:06:56 +0800

    完成語系選單

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -11,6 +11,30 @@ export class LoginComponent implements OnInit {
   account = '';
   password = '';
   remember = false;
+  langShow = false;
+  langChoosed = 'EN';
+  langList = [
+    {
+      lang: 'EN',
+      img: '/assets/imgs/iconLanEn.png',
+      label: 'ENGLISH'
+    },
+    {
+      lang: 'CN',
+      img: '/assets/imgs/iconLanSc.png',
+      label: '简体中文'
+    },
+    {
+      lang: 'ES',
+      img: '/assets/imgs/iconLanEs.png',
+      label: 'Español'
+    },
+    {
+      lang: 'PT',
+      img: '/assets/imgs/iconLanSc.png',
+      label: 'Português'
+    }
+  ]
   constructor(public route: Router) { }
 
   ngOnInit() {
@@ -22,6 +46,16 @@ export class LoginComponent implements OnInit {
     return { display: displayVal };
   }
   toggleRemeberValue() {this.remember = !this.remember; }
+  langSelectOnShow() {
+    this.langShow = ! this.langShow;
+  }
+  getLang() {
+    return this.langList.filter( e => e.lang === this.langChoosed )[0];
+  }
+  setLang(lang: string) {
+    this.langChoosed = lang;
+  }
+
   login() {
     console.log('account: ', this.account);
     if (this.account === '123' && this.password === '123') {
@@ -31,17 +65,17 @@ export class LoginComponent implements OnInit {
   }
 
   displayNotification() {
-    if (Notification.permission === 'granted') {
+    if (!('Notification' in window)) {
+      console.log('Notification not support');
+    } else if (Notification.permission === 'granted') {
       navigator.serviceWorker.getRegistration().then(reg => {
-        if (reg === undefined) {console.log('ServiceWorker are denied'); return; }
+        if (reg === undefined) { console.log('ServiceWorker are denied'); return; }
         const options = {
           icon: 'assets/icons/jyd-128x128.png',
           body: 'Have fun in JYD world!!',
           image: 'assets/icons/jyd-128x128.png'
         };
-        console.log(JSON.stringify(reg));
         reg.showNotification('Wellcome to JYD', options);
-        console.log(reg);
       });
     } else {
       console.log('Notification are denied');
