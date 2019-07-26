a9a03cd26208afbed7bfaf3899891a271e41d429
Yan-Hau <robert93327@gmail.com>
2019-07-26 23:16:21 +0800

    更新API (佳均的Azure)

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,10 +1,10 @@
 const Api = {
    /*
-    140.118.127.154
     UICD348EE55BC1A63AA805F63DE86EB5F7757D2EAD891FDFB25E5A523B26DE9E25D3D9D3952F505B6E4F157652ED50A0342F79CCFD79E70506
     13FCD060F0651739050F60CACBE5C33DD1BA68974831BAC0
+    https://jyddev.azurewebsites.net/
+    https://jyddev.azurewebsites.net/
    */
-
    /**
     * @method Post
     * @headers {'Content-Type':'application/json'}
@@ -12,7 +12,7 @@ const Api = {
     * @var  Password :string
     * @return UserID , UID
     */
-  loginApi      : 'http://140.118.127.154:6907/api/app/LoginAPI.ashx',
+  loginApi      : 'https://jyddev.azurewebsites.net/api/app/LoginAPI.ashx',
 
    /**
     * @method Get
@@ -20,7 +20,7 @@ const Api = {
     * @var void
     * @return GameList[]
     */
-  gameListApi   : 'http://140.118.127.154:6907/api/app/GameListAPI.aspx',
+  gameListApi   : 'https://jyddev.azurewebsites.net/api/app/GameListAPI.aspx',
 
    /**
     * @method Get
@@ -30,7 +30,7 @@ const Api = {
     * @var EndDate   :Y-m-d
     * @return RecordList[]
     */
-  betRecordApi  : 'http://140.118.127.154:6907/api/app/BetRecordAPI.ashx',
+  betRecordApi  : 'https://jyddev.azurewebsites.net/api/app/BetRecordAPI.ashx',
 
    /**
     * @method Get
@@ -40,7 +40,7 @@ const Api = {
     * @var EndDate   :Y-m-d
     * @return RecordList[]
     */
-  gameRecordApi : 'http://140.118.127.154:6907/api/app/GameRecordAPI.ashx',
+  gameRecordApi : 'https://jyddev.azurewebsites.net/api/app/GameRecordAPI.ashx',
 
    /**
     * @method Get
@@ -50,7 +50,7 @@ const Api = {
     * @var void
     * @return data[]
     */
-  amountApi     : 'http://140.118.127.154:6907/api/AmountAPI.aspx',
+  amountApi     : 'https://jyddev.azurewebsites.net/api/AmountAPI.aspx',
 
    /**
     * @method Get
