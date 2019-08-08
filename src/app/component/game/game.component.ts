8c60a5855cdeeab283ec909e1d1d39acc43e726a
106roger <imbigking12@gmail.com>
2019-08-08 22:31:57 +0800

    修正遊戲在pc顯示為全螢幕

--- src/app/component/game/game.component.ts
+++ src/app/component/game/game.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit , OnDestroy } from '@angular/core';
+import { Component, OnInit , OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthGuardService } from './../../services/auth-guard.service';
 // tslint:disable: no-string-literal
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit , OnDestroy {
-
-  constructor(private routerInfo: ActivatedRoute, private auth: AuthGuardService) { }
+export class GameComponent implements OnInit , OnDestroy, AfterViewInit {
+  @ViewChild('myiframe',{static: false}) myframe: ElementRef;
+  constructor(private routerInfo: ActivatedRoute, private auth: AuthGuardService, public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     console.log( this.routerInfo.snapshot.params.gameName );
     window['_GameName'] = this.routerInfo.snapshot.params.gameName;
     window['_GameUrl']  = 'https://dev-slot-mario.gd888.cc/gamelab/';
     window['_Bearer']   = this.auth.getUserID();
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  getSrc() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl('/assets/Games/embed.html');
+    if (screen.width > 500) {
+      return this.sanitizer.bypassSecurityTrustResourceUrl('/assets/Games/desktop.html');
+    } else {
+      return this.sanitizer.bypassSecurityTrustResourceUrl('/assets/Games/embed.html');
+    }
   }
 
   ngOnDestroy() {
